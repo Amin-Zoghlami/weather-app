9761a2efe41e489e2f2793ccd6c5e7a80c6bfec5
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2025-01-15 22:07:18 -0800

    Finish Weather App

--- src/display.js
+++ src/display.js
@@ -1,5 +1,26 @@
+import clearDayImage from "./img/clear-day.png";
+import clearNightImage from "./img/clear-night.png";
+import cloudyImage from "./img/cloudy.png";
+import fogImage from "./img/fog.png";
+import partlyCloudyDayImage from "./img/partly-cloudy-day.png";
+import partlyCloudyNightImage from "./img/partly-cloudy-night.png";
+import rainImage from "./img/rain.png";
+import snowImage from "./img/snow.png";
+import windImage from "./img/wind.png";
+
 export default function updateDisplay(info) {
-  console.log(info);
+  const iconImages = {
+    "clear-day": clearDayImage,
+    "clear-night": clearNightImage,
+    cloudy: cloudyImage,
+    fog: fogImage,
+    "partly-cloudy-day": partlyCloudyDayImage,
+    "partly-cloudy-night": partlyCloudyNightImage,
+    rain: rainImage,
+    snow: snowImage,
+    wind: windImage,
+  };
+
   const addressDisplay = document.getElementById("address");
   const iconDisplay = document.getElementById("icon");
   const tempDisplay = document.getElementById("temp");
@@ -9,7 +30,7 @@ export default function updateDisplay(info) {
   const windspeedDisplay = document.getElementById("windspeed");
 
   addressDisplay.textContent = info.address;
-  iconDisplay.src = `./img/${info.icon}.svg`;
+  iconDisplay.src = iconImages[info.icon];
   iconDisplay.alt = info.icon;
   tempDisplay.textContent = `${info.temp}°F`;
   feelslikeDisplay.textContent = `Feels like ${info.feelslike}°F`;
--- src/input.js
+++ src/input.js
@@ -6,12 +6,22 @@ export default (function addInputs() {
 
   const searchInput = document.getElementById("search");
   const searchButton = document.querySelector(".submit");
+
+  searchInput.addEventListener("input", () => {
+    searchInput.setCustomValidity("");
+    searchInput.reportValidity();
+  });
+
   searchButton.addEventListener("click", (event) => {
     event.preventDefault();
     const location = searchInput.value;
     app
       .getData(location)
       .then((data) => app.processData(data))
-      .then((info) => updateDisplay(info));
+      .then((info) => updateDisplay(info))
+      .catch(() => {
+        searchInput.setCustomValidity("Location not found");
+        searchInput.reportValidity();
+      });
   });
 });
